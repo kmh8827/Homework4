63660ea63a655a7bc95b20b6a226dbc7dbe072d3
kevin <kevinhellman94@gmail.com>
2020-12-20 18:59:39 -0500

    added styling to buttons as well as shortened code significantly

--- script.js
+++ script.js
@@ -1,154 +1,112 @@
-// //List of Questions that can appear on the quiz
 var questions = [];
 
-
-    var question1 = [
+// //List of Questions that can appear on the quiz
+var question1 = [
     "If I want to conver the string 1.325 to a number what should I use?",
     "ParseInt()",
     "ParseFloat()",
     "String.push()",
     "JSON.stringify()"
-    ];
-    var question2 = [
+];
+var question2 = [
     "What will the array.length(fruits) return given fruits['apple','banana','orange','pear'];?",
     "4",
     "3",
     "5",
     "1"
-    ];
-    var question3 = [
+];
+var question3 = [
     "What will array.push('pumpkin') do to the array vegetables['potato'];?",
     "['potato','pumpkin'];",
     "['potato pumpkin'];",
     "['pumpkin','potato']",
     "['pumpkin potato']"
-    ];
-    var question4 = [
+];
+var question4 = [
     "What is the difference between a global variable and a local variable in Javascript?",
     "A global variable always has the same value",
     "A global variable is only usable in certain areas while a global variable is usable everywhere",
     "local variables are outside of functions while global variables are inside of functions",
     "a global variable appears before all other scripts so it can be accessed anywhere while local variables are not"
-    ];
-    var question5 = [
+];
+var question5 = [
     "What is the appropriate way to generate a random number between 0 and 5",
     "Math.floor(Math.random()) * 5",
     "math.floor(math.random()) * 6",
     "Math.floor(Math.random()) * 6",
     "Math.ceiling(Math.random()) * 6"
-    ];
+];
 
-//Randomly Select the question
+//Creates buttons, styles buttons and pushes content to buttons
 function ThisQuestion(questionPicked) {
     questionPicked = parseInt(questionPicked);
-    if (questionPicked === 1) {
 
-        var questionAsk = document.createElement("p");
-        var option1 = document.createElement("button");
-        var option2 = document.createElement("button");
-        var option3 = document.createElement("button");
-        var option4 = document.createElement("button");
+    var questionAsk = document.createElement("h2");
+    var option1 = document.createElement("button");
+    var option2 = document.createElement("button");
+    var option3 = document.createElement("button");
+    var option4 = document.createElement("button");
+
+    option1.setAttribute("class","btn btn-primary btn-lg btn-block");
+    option1.setAttribute("style","width: 60%;margin: 0 0 5px 5px");
+    option2.setAttribute("class","btn btn-primary btn-lg btn-block");
+    option2.setAttribute("style","width: 60%;margin: 0 0 5px 5px");
+    option3.setAttribute("class","btn btn-primary btn-lg btn-block");
+    option3.setAttribute("style","width: 60%;margin: 0 0 5px 5px");
+    option4.setAttribute("class","btn btn-primary btn-lg btn-block");
+    option4.setAttribute("style","width: 60%;margin: 0 0 5px 5px");
+
+    if (questionPicked === 1) {
 
         questionAsk.textContent = (question1[0]);
         option1.textContent = (question1[1]);
         option2.textContent = (question1[2]);
         option3.textContent = (question1[3]);
         option4.textContent = (question1[4]);
-        console.log("It made it here YAY" + question1[0]);
-        
-        document.body.append(questionAsk);
-        document.body.append(option1);
-        document.body.append(option2);
-        document.body.append(option3);
-        document.body.append(option4);
 
     } else  if (questionPicked === 2) {
-
-        var questionAsk = document.createElement("p");
-        var option1 = document.createElement("button");
-        var option2 = document.createElement("button");
-        var option3 = document.createElement("button");
-        var option4 = document.createElement("button");
 
         questionAsk.textContent = (question2[0]);
         option1.textContent = (question2[1]);
         option2.textContent = (question2[2]);
         option3.textContent = (question2[3]);
         option4.textContent = (question2[4]);
-        console.log("It made it here YAY" + question2[0]);
-
-        document.body.append(questionAsk);
-        document.body.append(option1);
-        document.body.append(option2);
-        document.body.append(option3);
-        document.body.append(option4);
 
     } else  if (questionPicked === 3) {
-
-        var questionAsk = document.createElement("p");
-        var option1 = document.createElement("button");
-        var option2 = document.createElement("button");
-        var option3 = document.createElement("button");
-        var option4 = document.createElement("button");
 
         questionAsk.textContent = (question3[0]);
         option1.textContent = (question3[1]);
         option2.textContent = (question3[2]);
         option3.textContent = (question3[3]);
         option4.textContent = (question3[4]);
-        console.log("It made it here YAY" + question3[0]);
-
-        document.body.append(questionAsk);
-        document.body.append(option1);
-        document.body.append(option2);
-        document.body.append(option3);
-        document.body.append(option4);
 
     } else  if (questionPicked === 4) {
-
-        var questionAsk = document.createElement("p");
-        var option1 = document.createElement("button");
-        var option2 = document.createElement("button");
-        var option3 = document.createElement("button");
-        var option4 = document.createElement("button");
 
         questionAsk.textContent = (question4[0]);
         option1.textContent = (question4[1]);
         option2.textContent = (question4[2]);
         option3.textContent = (question4[3]);
         option4.textContent = (question4[4]);
-        console.log("It made it here YAY" + question4[0]);
 
-        document.body.append(questionAsk);
-        document.body.append(option1);
-        document.body.append(option2);
-        document.body.append(option3);
-        document.body.append(option4);
-        
     } else if (questionPicked === 5) {
-
-        var questionAsk = document.createElement("p");
-        var option1 = document.createElement("button");
-        var option2 = document.createElement("button");
-        var option3 = document.createElement("button");
-        var option4 = document.createElement("button");
 
         questionAsk.textContent = (question5[0]);
         option1.textContent = (question5[1]);
         option2.textContent = (question5[2]);
         option3.textContent = (question5[3]);
         option4.textContent = (question5[4]);
-
-        document.body.append(questionAsk);
-        document.body.append(option1);
-        document.body.append(option2);
-        document.body.append(option3);
-        document.body.append(option4);
-
-        console.log("It made it here YAY" + question5[0]);
     }
+
+    document.body.append(questionAsk);
+    document.body.append(option1);
+    document.body.append(option2);
+    document.body.append(option3);
+    document.body.append(option4);
 } 
+
     var questionOrder = [1,2,3,4,5];
+
+//Randomly Selects the question to use
 function whichQuestion() {
 while (questionOrder.length > 0) {
     var index = Math.floor(Math.random() * (questionOrder.length));
@@ -161,35 +119,22 @@ while (questionOrder.length > 0) {
 
 whichQuestion();
 
+//Figures out which code to question to push to buttons
 for (var i = 0; i < questions.length; i++)
 {
     if (questions[i] === 1) {
         ThisQuestion(questions[i]);
-        console.log("THIS VALUE IS " + questions[i]);
     }
     else if (questions[i] === 2) {
         ThisQuestion(questions[i]);
-        console.log("THIS VALUE IS " + questions[i]);
     }
     else if (questions[i] === 3) {
         ThisQuestion(questions[i]);
-        console.log("THIS VALUE IS " + questions[i]);
     }
     else if (questions[i] === 4) {
         ThisQuestion(questions[i]);
-        console.log("THIS VALUE IS " + questions[i]);
     }
     else if (questions[i] === 5) {
         ThisQuestion(questions[i]);
-        console.log("THIS VALUE IS " + questions[i]);
     }
 }
-
-
-//Push the specific question to the HTML buttons
-// for (var i = 0; i < questions.length; i++) {
-
-     
- 
-
-// }
